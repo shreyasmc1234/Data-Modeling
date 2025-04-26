7135976f9cfcb35aa4eccb78be7d4c44ea63d15a
SHREYAS M C <140893678+shreyasmc1234@users.noreply.github.com>
2025-04-26 19:09:33 +0530

    Create datamodel_code.sql

---
+++ amazonmusic/datamodel_code.sql
@@ -0,0 +1,44 @@
+create database music;
+create schema amazon;
+
+--Fact Table
+
+create table Orders(
+order_id int primary key,
+order_date date,
+order_amount decimal(20,4),
+order_mode string,
+customer_id int,
+booking_id int,
+author_id int,
+foreign key (customer_id) references customers(customer_id),
+foreign key (booking_id) references bookings(booking_id),
+foreign key (author_id) references authors(author_id),
+foreign key (customer_id) references reviews(reviewer_id)
+);
+
+--Dimension Tables
+
+create or replace table bookings(
+booking_id int primary key,
+book_name string,
+price decimal(20,4)
+);
+
+create or replace table authors(
+author_id int primary key,
+author_name string
+);
+
+create or replace table customers(
+customer_id int primary key,
+customer_name string,
+customer_place string
+);
+
+create or replace table reviews(
+reviewer_id int primary key,
+ratings int,
+reviews string
+);
+
